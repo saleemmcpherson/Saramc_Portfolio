2d064d2ea78538399e441ed373f62f223519c68e
saleemmcpherson <saleemmcpherson@yahoo.com>
2020-03-18 23:32:32 -0400

    imported header and footer components

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Footer extends Component {
+    render() {
+        return (
+            <div style={{backgroundColor: "red"}}>
+                This will be the footer of page 
+            </div>
+        );
+    }
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Header extends Component {
+    render() {
+        return (
+            <div style={{backgroundColor: "teal"}}>
+                This will be the header for site 
+            </div>
+        );
+    }
+}
+
+export default Header;
