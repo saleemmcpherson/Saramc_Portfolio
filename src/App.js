cf5e8193e4361e4bdc6f6d3ee32ea8a01c465d95
saleemmcpherson <saleemmcpherson@yahoo.com>
2020-03-23 15:27:05 -0400

    moved components to MainContainer.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Main from "./components/Main";
+import MainContainer from "./components/MainContainer";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Header title="This is where the header will be"/>
-        <Main title="This is where the main content will be"/>
-        <Footer title="This is where the footer will be"/>
+        <MainContainer title="This is where the main content will be"/>
       </div>
     );
   }
--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
 
 export class Main extends Component {
   render() {
     return (
       <div className="mainContainer" style={{ backgroundColor: "tomato" }}>
+        <Header title="this is where the header will be"/>
         {this.props.title}
+        <Footer title="this is where the footer will be"/>
       </div>
     );
   }
