668ac54c51ceab8c2a752ae0108e67071d1ea32f
saleemmcpherson <saleemmcpherson@yahoo.com>
2020-03-24 15:27:12 -0400

    working on passing values from local json to create sidenav component

---
+++ src/components/SideNav.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import socialMedia from "../data/socialMedia.json"
+
+export class SideNav extends Component {
+    render() {
+
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default SideNav
