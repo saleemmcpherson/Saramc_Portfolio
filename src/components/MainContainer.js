6419c08c4537b04aa4a93e9fd0aa38c5e6199e0b
saleemmcpherson <saleemmcpherson@yahoo.com>
2020-03-31 21:30:57 -0400

    moved components to seperate directories

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,16 +1,15 @@
 import React, { Component } from "react";
-// import Header from "./Header";
-// import Footer from "./Footer";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
 import SideNav from "./SideNav/SideNav";
 
 export class Main extends Component {
   render() {
     return (
       <div className="mainContainer">
-        {/* <Header title="this is where the header will be"/> */}
-        {/* {this.props.title} */}
-        {/* <Footer title="this is where the footer will be"/> */}
+        <Header title="this is where the header will be"/>
         <SideNav />
+        <Footer title="this is where the footer will be"/>
       </div>
     );
   }
