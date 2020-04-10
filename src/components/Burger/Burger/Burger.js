ed3c9bb0df944bdf3f5f589cd7b536dda2e278d0
saleemmcpherson <saleemmcpherson@yahoo.com>
2020-04-09 23:04:47 -0400

    started burger menu

---
+++ src/components/Burger/Burger/Burger.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyledBurger } from "./Burger.styled";
+
+const Burger = () => {
+  return (
+    <StyledBurger>
+      <span />
+      <span />
+      <span />
+    </StyledBurger>
+  );
+};
+
+export default Burger;
---
+++ src/components/Burger/Menu/Menu.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+const Menu = () => {
+  return <div>
+      
+  </div>;
+};
+
+export default Menu;
