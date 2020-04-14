65dcbc46a5888c8cbe0acc180decf3a94ca50fc9
saleemmcpherson <saleemmcpherson@yahoo.com>
2020-04-14 14:33:28 -0400

    trying to get li tags spread evenly across footer

--- src/components/Footer/Footer.styled.js
+++ src/components/Footer/Footer.styled.js
@@ -2,15 +2,7 @@ import styled from "styled-components";
 
 export const StyledFooter = styled.div`
 display: flex;
-justify-content: center;
-align-items: center;
 position: absolute;
 bottom: 0;
-
-ul{
-    display: flex;
-    margins: 0;
-    
-}
 
 `;
